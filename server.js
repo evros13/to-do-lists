b14a50516b1be2875ae9c4974edcbe38b7120041
Edna Vidal <ednavidalros@gmail.com>
2023-11-23 12:56:28 +0100

    "Server and DB tested and working"

---
+++ server.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const app = express()
+
+
+// CONNECTING TO THE DB
+const mongoose = require('mongoose')
+const MONGO_URI = process.env.MONGODB_URI 
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    const dbName = x.connections[0].name;
+    console.log(`Connected to Mongo! Database name: "${dbName}"`)
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err)
+  })
+
+
+
+// CONNECTING THE SERVER
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`)
+})
+
